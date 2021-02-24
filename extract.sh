b1b485a7b14537fed8f6056c7810f07f4c2ada11
RubénEu <garcia.ruben@uma.es>
2021-02-24 22:59:42 +0100

    Removed interpreter in the extract.sh because it needs Java 13.

--- extract.sh
+++ extract.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 mkdir while-java-compiler
-cp -r src/main/java/* while-java-compiler/
+cp src/main/java/WHILEC.java while-java-compiler/
+cp -r src/main/java/ast/ while-java-compiler/
+cp -r src/main/java/ctd/ while-java-compiler/
+cp -r src/main/java/visitor/ while-java-compiler/
+cp -r src/main/java/WHILEC.java while-java-compiler/
+cp -r src/main/java/WHILEC.java while-java-compiler/
 cp src/main/cup/parser.cup while-java-compiler/WHILEC.cup
 cp src/main/jflex/lexer.jflex while-java-compiler/WHILEC.flex
 cp -r src/main/resources/code-examples/ while-java-compiler/
